f0dbe63d62374dbd7e2115c074bc59b1d9317988
KritametPP <kritamet.p@gmail.com>
2024-07-02 11:24:24 +0700

    authRoute

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require('dontenv').config()
 const express = require('express')
 const notFound = require('./middlewares/not-found')
 const errorMw = require('./middlewares/error-mw')
+const authRoute = require('./routes/auth-route')
 const app = express()
 
 app.use(notFound)
 app.use(errorMw)
+app.use('/auth', authRoute)
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8888
 app.listen(port, () => console.log('Server running', port))
