d2611ab288e2f90d4488cc8183fe296f32ec1425
KritametPP <kritamet.p@gmail.com>
2024-07-02 11:29:08 +0700

    todoRoute

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require('dontenv').config()
 const express = require('express')
 const notFound = require('./middlewares/not-found')
 const errorMw = require('./middlewares/error-mw')
+const todoRoute = require('./routes/todo-route')
 const app = express()
 
+app.use('/todo', todoRoute)
 app.use(notFound)
 app.use(errorMw)
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 9999
 app.listen(port, () => console.log('Server running', port))
